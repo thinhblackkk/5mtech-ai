5dfe4e0b4a41484aee71c49073d4673717a47507
huynhphatthinh <huynhphatthinh2005@example.com>
2026-08-07 23:26:42 +0700

    feat: add 5mtech personality system prompt

--- fivemtech_ai.py
+++ fivemtech_ai.py
@@ -1,6 +1,8 @@
 from google import genai
 import os
 
+from fivemtech_config import SYSTEM_PROMPT
+
 
 class FiveMTechAI:
 
@@ -10,6 +12,16 @@ class FiveMTechAI:
         )
 
     def ask(self, contents):
+
+        contents.insert(0, {
+            "role": "user",
+            "parts": [
+                {
+                    "text": SYSTEM_PROMPT
+                }
+            ]
+        })
+
         response = self.client.models.generate_content(
             model="gemini-3-flash-preview",
             contents=contents
